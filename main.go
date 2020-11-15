7c0fa06cbdc93bfae32fd02933b6b0ed54aa18f2
SnowyPainter <SnowyPainter@gmail.com>
2020-11-15 16:56:52 +0900

    Update main.go for all kinds of functions

--- main.go
+++ main.go
@@ -7,19 +7,32 @@ import (
 )
 
 func main() {
-	m := matrix.NewMatrix(2, 3)
-	m.SetRow(0, []float64{1, 2})
-	m.SetRow(1, []float64{3, 4})
-	m.SetRow(2, []float64{5, 6})
+	/*
+		WARN : variables are not a car!
+	*/
 
-	//m.Reset([][]float64{{1, 2}, {3, 4}, {5, 6}})
+	m := matrix.NewMatrix(3, 3)
+	m.Reset([][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
+	m2 := matrix.NewMatrix(3, 3)
+	m2.Reset([][]float64{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})
 
-	m2 := matrix.NewMatrix(2, 2)
-	m2.SetRow(0, []float64{1, 3})
-	m2.SetRow(1, []float64{2, 4})
+	m3, err := m.Plus(m2)
+	fmt.Println("m + m2 = ", m3)
 
-	fmt.Println(m)
-	fmt.Println(m.Transpose())
+	m4, err := m.Minus(m2)
+	fmt.Println("m - m2 = ", m4)
+
+	m5, err := m.Multiply(m2)
+	fmt.Println("m * m2 = ", m5)
+
+	m6 := m.ScalarMultiply(5)
+	fmt.Println("|m| * |5| = ", m6)
+
+	fmt.Println("m.Transpose() = ", m.Transpose())
+
+	if err != nil {
+		return
+	}
 
 	return
 }
